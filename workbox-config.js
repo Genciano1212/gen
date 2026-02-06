3d95bb250c520b4e01097d9f1a57c3594e8c7f3f
Genciano <1212genci@gmail.com>
2026-02-06 11:13:06 -0400

    feat: simplify accumulation logic to 4 tiers and bump version to v3.6

--- workbox-config.js
+++ workbox-config.js
@@ -3,7 +3,7 @@ module.exports = {
   globPatterns: [
     '**/*.{html,js,css,png,svg,ico,json}'
   ],
-  swDest: 'service-worker-v221.js',
+  swDest: 'service-worker-v222.js',
   runtimeCaching: [
     {
       urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
