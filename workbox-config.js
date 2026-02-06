38efb9b3e6b0a2f754c6e3ce8fd7e032db65c60d
Genciano <1212genci@gmail.com>
2026-02-06 12:46:12 -0400

    fix(preview): correct calculation logic and update to v1.3

--- workbox-config.js
+++ workbox-config.js
@@ -3,7 +3,7 @@ module.exports = {
   globPatterns: [
     '**/*.{html,js,css,png,svg,ico,json}'
   ],
-  swDest: 'service-worker-v227.js',
+  swDest: 'service-worker-v228.js',
   runtimeCaching: [
     {
       urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
