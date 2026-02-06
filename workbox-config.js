f2b69c1579c970d86d41b3027641340041b50b0f
Genciano <1212genci@gmail.com>
2026-02-06 11:38:56 -0400

    chore: bump version to v1.2 and service worker to v227

--- workbox-config.js
+++ workbox-config.js
@@ -3,7 +3,7 @@ module.exports = {
   globPatterns: [
     '**/*.{html,js,css,png,svg,ico,json}'
   ],
-  swDest: 'service-worker-v226.js',
+  swDest: 'service-worker-v227.js',
   runtimeCaching: [
     {
       urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
