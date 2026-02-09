ded31069027ded3dbf7884f89b746979664ac443
Genciano <1212genci@gmail.com>
2026-02-09 17:06:14 -0400

    feat: upgrade to v1.3

--- workbox-config.js
+++ workbox-config.js
@@ -3,7 +3,7 @@ module.exports = {
   globPatterns: [
     '**/*.{html,js,css,png,svg,ico,json}'
   ],
-  swDest: 'service-worker-v120.js',
+  swDest: 'service-worker-v130.js',
   runtimeCaching: [
     {
       urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
